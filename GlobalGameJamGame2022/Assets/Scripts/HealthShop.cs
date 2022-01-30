c2ef72577ff97f53a75a022468ce47c53107d71b
FluffyMichael <michael.mvdsanden@gmail.com>
2022-01-30 10:05:50 +0100

    Health shop
    Nog zonder de health daadwerkelijk aan te passen

---
+++ GlobalGameJamGame2022/Assets/Scripts/HealthShop.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HealthShop : MonoBehaviour
+{
+    [SerializeField] private Coins coins;
+    [SerializeField] private HealthShopTrigger trigger;
+
+    public void buyHealth()
+    {
+        if (coins.coinAmount >= 4)
+        {
+            coins.changeCoins(-4);
+            //health van speler + 1
+            Debug.Log("health + 1");
+        }
+        else
+        {
+            trigger.OpenWarning();
+        }
+    } 
+}
