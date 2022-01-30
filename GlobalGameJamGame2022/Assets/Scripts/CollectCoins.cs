faed0ab8e7bbf1eae1f915f4e16e73c8a897a82f
PieterWinderlichSchool <pieterwinderlich@outlook.com>
2022-01-30 14:01:21 +0100

    Changed Player Collision + added win condition
    Changed player collision by adding a child with a 2d collider instead of on the player. Added a win condition for gathering all the coins and killing all enemies.

--- GlobalGameJamGame2022/Assets/Scripts/CollectCoins.cs
+++ GlobalGameJamGame2022/Assets/Scripts/CollectCoins.cs
@@ -5,12 +5,14 @@ using UnityEngine;
 public class CollectCoins : MonoBehaviour
 {
 	[SerializeField] private Coins coins;
+	[SerializeField] private CheckWinCondition winCondition;
 
 	private void OnTriggerEnter2D(Collider2D collision)
 	{
 		if(collision.CompareTag("Coins"))
 		{
 			coins.changeCoins(1);
+			winCondition.coins += 1;
 			collision.gameObject.SetActive(false);
 		}
 	}
--- GlobalGameJamGame2022/Assets/Scripts/ModeSwitch.cs
+++ GlobalGameJamGame2022/Assets/Scripts/ModeSwitch.cs
@@ -95,4 +95,9 @@ public class ModeSwitch : MonoBehaviour
             }
         }  
     }
+
+    public List<GameObject> getHeavenEnemies()
+    {
+        return HeavenCreatures;
+    }
 }
